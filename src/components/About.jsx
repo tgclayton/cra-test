ad9614d7663a78fadf5803c1974fe2d040eea738
Tom Clayton <tgclayton1@gmail.com>
2020-08-18 11:52:25 +1200

    add more about text

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -9,6 +9,10 @@ export default function About() {
       <p>A simple app written to practice using Create-react-App and to learn React hooks.
       In the course of writing it I also learned about localStorage and used that as well.
       </p>
+      <p>
+        Typing in a new to-do and pressing enter will add a new one, while clicking on 
+        an active to-do will allow the text to be edited. Duplicate text in to-dos is not allowed.
+      </p>
       </div>
     </div>
   )
