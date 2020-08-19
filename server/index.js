e6f8ebe29d140020a6c356f34cc0298b772e2dec
Tom Clayton <tgclayton1@gmail.com>
2020-08-20 11:11:04 +1200

    add a server

--- server/index.js
+++ server/index.js
@@ -1,5 +1,5 @@
 const server = require('./server')
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 3001
 
 server.listen(port, function () {
   // eslint-disable-next-line no-console
--- server/server.js
+++ server/server.js
@@ -5,8 +5,4 @@ const server = express()
 server.use(express.json())
 server.use(express.static(path.join(__dirname, './public')))
 
-// server.use(express.static('public'))
-// server.use('/', home)
-// server.use('/saves', saves)
-
 module.exports = server
