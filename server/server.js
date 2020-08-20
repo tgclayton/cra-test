a056d3e5879d7b53f04fe6e7504fb47fc7569cc1
Tom Clayton <tgclayton1@gmail.com>
2020-08-20 16:32:09 +1200

    restore basic server listenting on port 3001

--- server/server.js
+++ server/server.js
@@ -1,14 +1,13 @@
 const path = require('path')
 const express = require('express')
 const server = express()
+const home = require('./routes/home')
 // const users = require('../routes/users')
-// const data = require('../routes/data')
 
 server.use(express.json())
 console.log('dirname:', __dirname)
-server.use(express.static(path.join(__dirname, '../public')))
+server.use(express.static(path.join(__dirname, 'build')))
 
-// server.use('/users', users)
-// server.use('/data', data)
+server.use('/', home)
 
 module.exports = server
