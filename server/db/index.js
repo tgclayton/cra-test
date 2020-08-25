47350108284580744b2306e6f3132559b9987483
Tom Clayton <tgclayton1@gmail.com>
2020-08-25 14:46:28 +1200

    make connections to specific databases

--- server/db/index.js
+++ server/db/index.js
@@ -2,12 +2,20 @@
 const MongoClient = require('mongodb').MongoClient;
 const uri = "mongodb+srv://Tom:LzIz1C5CA5Poh9Vl@toms-to-do.zkt6p.mongodb.net/todo-users?retryWrites=true&w=majority";
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+const connection = client
+    .connect()
+    .then(() => { console.log('Connected to mongoDB') })
+    .catch(e => {
+        console.error('Connection error', e.message)
+    })
 
-const db = client
-   .connect()
-   .then(() => {console.log('Connected to mongoDB')})
-   .catch(e => {
-    console.error('Connection error', e.message)
-})
+const users = client.connect('users')
+const todos = client.connect('todos')
 
-module.exports = db
+module.exports = {
+
+}
+
+function logIn(username) {
+
+}
--- src/components/About.jsx
+++ src/components/About.jsx
@@ -2,17 +2,17 @@ import React from 'react'
 import '../styles/About.css'
 
 export default function About(props) {
-props.setPage('about')
+  props.setPage('about')
 
   return (
-    <div id = 'about-page' className = 'full-page'>
-      <div id = 'about-window' className = "todo-list">
-      <p>A simple app written to practice using Create-react-App and to learn React hooks.
-      App curently uses local storage to hold to-do data.
+    <div id='about-page' className='full-page'>
+      <div id='about-window' className="todo-list">
+        <p>A simple app written to practice using Create-react-App and to learn React hooks.
+        App curently uses local storage to hold to-do data.
       </p>
-      <p>
-        Typing in a new to-do and pressing enter will add a new one, while clicking on 
-        an active to-do will allow the text to be edited. Duplicate text in to-dos is not allowed.
+        <p>
+          Typing in a new to-do and pressing enter will add a new one, while clicking on
+          an active to-do will allow the text to be edited. Duplicate text in to-dos is not allowed.
       </p>
       </div>
     </div>
