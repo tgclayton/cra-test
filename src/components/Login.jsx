5b9d498f557ae5b299edb4da13dd399881766299
Tom Clayton <tgclayton1@gmail.com>
2020-08-26 14:36:31 +1200

    create login/signup functions

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -3,10 +3,32 @@ import '../styles/Login.css'
 
 export default function Login(props) {
 
+  function getDetails() {
+    const password = document.getElementById('password-input').value
+    const username = document.getElementById('username-input').value
+    return {password, username}
+  }
+
+  async function login () {
+    const {password, username} = getDetails()
+  }
+
+  async function signUp () {
+    const {password, username} = getDetails()
+  }
+
   return (
     <div id='login-window' className='ivory-back curvy-border'>
-      <button>Login</button>
-      <button>Sign Up</button>
+    <h2 style = {{margin: 0}}>Login</h2>
+      <label for = 'userInp'>Username</label>
+      <input id = 'username-input' name = 'userInp' className='login-input' type = 'text'></input>
+      <label for = 'passInp'>Password</label>
+      <input id = 'password-input' name = 'passInp' className='login-input' type = 'text'></input>
+      <br/>
+      <div className = 'flex-row'>
+        <button onClick = {() => login()}>Login</button>
+        <button onClick = {() => signUp()}>Sign Up</button>
+      </div>
     </div>
   )
 }
