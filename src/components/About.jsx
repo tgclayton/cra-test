9c808bd4f2000cd830a3da8906bb93d0b33b2b12
Tom Clayton <tgclayton1@gmail.com>
2020-08-31 12:47:11 +1200

    login function works correctly

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -8,7 +8,7 @@ export default function About(props) {
     <div id='about-page' className='full-page'>
       <div id='about-window' className="todo-list">
         <p>A simple app written to practice using Create-react-App and to learn React hooks.
-        App curently uses local storage to hold to-do data.
+        App uses Mongo Atlas for data handling, login feature is a placeholder and not is secure.
       </p>
         <p>
           Typing in a new to-do and pressing enter will add a new one, while clicking on
